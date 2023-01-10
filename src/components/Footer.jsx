8689368e217a704fc511b4e7143cafe71a1abd05
theodaguier <92515854+theodaguier@users.noreply.github.com>
2023-01-10 02:02:33 +0100

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,14 @@ import arrowUB from "./../assets/icons/arrow.up.black.svg";
 import arrowUW from "./../assets/icons/arrow.up.white.svg";
 
 function Footer({ isLight }) {
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div className="footer">
       <p>© 2023 Cristina Casañas. All rights reserved </p>
-      <div className="back-to-top">
+      <div className="back-to-top" onClick={handleClick}>
         Back to top{" "}
         <img
           className="icon-up"
