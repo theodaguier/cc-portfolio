31c5806d408d796bce65f67d8d0ff727ac289636
theodaguier <92515854+theodaguier@users.noreply.github.com>
2023-01-14 00:25:52 +0100

    Update

--- src/data/images_gallery.js
+++ src/data/images_gallery.js
@@ -1,39 +1,39 @@
 const images_gallery = [
   {
-    src: "src/assets/img/i1.jpg",
+    src: "./img/i1.jpg",
   },
   {
-    src: "src/assets/img/i2.jpg",
+    src: "./img/i2.jpg",
   },
   {
-    src: "src/assets/img/i3.jpg",
+    src: "./img/i3.jpg",
   },
   {
-    src: "src/assets/img/i4.jpg",
+    src: "./img/i4.jpg",
   },
   {
-    src: "src/assets/img/i5.jpg",
+    src: "./img/i5.jpg",
   },
   {
-    src: "src/assets/img/i6.jpg",
+    src: "./img/i6.jpg",
   },
   {
-    src: "src/assets/img/i7.jpg",
+    src: "./img/i7.jpg",
   },
   {
-    src: "src/assets/img/i8.jpg",
+    src: "./img/i8.jpg",
   },
   {
-    src: "src/assets/img/i9.jpg",
+    src: "./img/i9.jpg",
   },
   {
-    src: "src/assets/img/i10.jpg",
+    src: "./img/i10.jpg",
   },
   {
-    src: "src/assets/img/i11.jpg",
+    src: "./img/i11.jpg",
   },
   {
-    src: "src/assets/img/i12.jpg",
+    src: "./img/i12.jpg",
   },
 ];
 
