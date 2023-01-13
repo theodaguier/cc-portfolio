5751417f079b58d27ed0c0f7179620d73117a8fe
theodaguier <92515854+theodaguier@users.noreply.github.com>
2023-01-13 23:56:59 +0100

    Update

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ const projects_data = [
     name: "Project 1",
     description:
       "Il faisait un temps magnifique ce matin-là. Le soleil brillait haut dans le ciel et les oiseaux chantaient joyeusement. J'ai décidé de profiter de cette belle journée en prenant une promenade dans les bois qui bordaient notre maison.Le sentier était paisible et j'ai pu savourer chaque instant de cette balade. J'ai admiré les arbres, les fleurs et les animaux que j'ai croisés sur ma route. J'ai respiré l'air frais et pur, et j'ai senti mon esprit se libérer de toutes les tensions de la vie quotidienne.",
-    images: ["public/img/i1.jpg"],
+    images: ["src/assets/img/i1.jpg"],
     year: 2020,
     size: "Large",
     author: "John Doe",
@@ -13,7 +13,7 @@ const projects_data = [
     name: "Project 2",
     description:
       "Il faisait un temps magnifique ce matin-là. Le soleil brillait haut dans le ciel et les oiseaux chantaient joyeusement. J'ai décidé de profiter de cette belle journée en prenant une promenade dans les bois qui bordaient notre maison.Le sentier était paisible et j'ai pu savourer chaque instant de cette balade. J'ai admiré les arbres, les fleurs et les animaux que j'ai croisés sur ma route. J'ai respiré l'air frais et pur, et j'ai senti mon esprit se libérer de toutes les tensions de la vie quotidienne.",
-    images: ["./../../public/img/i33.jpg"],
+    images: ["src/assets/img/i2.jpg"],
     year: 2020,
     size: "Large",
     author: "John Doe",
