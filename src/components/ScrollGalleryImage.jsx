12ef5930b6e2040d83d24d01505403d55749907d
theodaguier <92515854+theodaguier@users.noreply.github.com>
2023-02-03 17:52:13 +0100

    Update scroll gallery

--- src/components/ScrollGalleryImage.jsx
+++ src/components/ScrollGalleryImage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 
 import "../styles/pages/gallery.scss";
 import shortid from "shortid";
@@ -6,10 +6,10 @@ import shortid from "shortid";
 import images_gallery from "../data/images_gallery.js";
 
 function ScrollGalleryImage({ onImageClick, setIsHovering }) {
-  const [images, setImages] = useState(images_gallery);
-  const imagesContainerRef = useRef(null);
-  const [index, setIndex] = useState(0);
+  const [images, setImages] = useState(images_gallery.slice(0, 12));
+  const [index, setIndex] = useState(12);
   const [hasMore, setHasMore] = useState(true);
+  const imagesContainerRef = useRef(null);
 
   useEffect(() => {
     if (index >= images_gallery.length) {
@@ -17,53 +17,45 @@ function ScrollGalleryImage({ onImageClick, setIsHovering }) {
     }
   }, [index]);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && hasMore) {
+          fetchMore();
+        }
+      });
+    });
+    observer.observe(imagesContainerRef.current);
+    return () => observer.disconnect();
+  }, [hasMore, imagesContainerRef]);
+
   const fetchMore = () => {
     if (index >= images_gallery.length) {
       setIndex(0);
-      setHasMore(false);
-    } else {
-      setImages((prevImages) => [
-        ...prevImages,
-        ...images_gallery.slice(index, index + 12),
-      ]);
-      setIndex((prevIndex) => prevIndex + 12);
     }
-  };
-
-  const imageRefs = useRef([]).current;
-  const addImageRef = (imageRef) => {
-    imageRefs.push(imageRef);
+    setImages((prevImages) => [
+      ...prevImages,
+      ...images_gallery.slice(index, index + 12),
+    ]);
+    setIndex((prevIndex) => prevIndex + 12);
   };
 
   const handleMouseOver = (index) => {
     setIsHovering(true);
-    imageRefs[index].current.classList.add("hover");
+    images[index].isHovering = true;
+    setImages([...images]);
   };
 
   const handleMouseOut = (index) => {
     setIsHovering(false);
-    imageRefs[index].current.classList.remove("hover");
+    images[index].isHovering = false;
+    setImages([...images]);
   };
-
-  const handleScroll = () => {
-    const el = imagesContainerRef.current;
-    if (el) {
-      const { scrollTop, clientHeight, scrollHeight } = el;
-      if (scrollTop + clientHeight >= scrollHeight && hasMore) {
-        fetchMore();
-      }
-    }
-  };
-
   return (
-    <div
-      className="scroll-gallery"
-      ref={imagesContainerRef}
-      onScroll={handleScroll}
-    >
+    <div className="scroll-gallery" ref={imagesContainerRef}>
       {images.map((image, index) => {
         const imageRef = useRef(null);
-        addImageRef(imageRef);
+
         return (
           <img
             ref={imageRef}
