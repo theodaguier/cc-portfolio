d6bad390cdeaf58e620e87e3c650f4b8f4507507
theodaguier <92515854+theodaguier@users.noreply.github.com>
2023-02-16 16:24:20 +0100

    fixing galery scroll

--- src/components/ScrollGalleryImage.jsx
+++ src/components/ScrollGalleryImage.jsx
@@ -1,66 +1,70 @@
 import React, { useEffect, useRef } from "react";
 import shortid from "shortid";
-import Lenis from "@studio-freight/lenis";
 import images_gallery from "../data/images_gallery.js";
 
-const ScrollGalleryImage = ({ children, smooth, infinite }) => {
-  const rafRef = useRef(null);
+const ScrollGalleryImage = () => {
+  // cette galerie d'image importés depuis un fichier json
+  // cette galerie doit un etre un scroll vertical
+  // la galerie doit repeter à l'infini les images
+  // avec un effet de smooth scroll
+
+  const scrollGallery = useRef(null);
 
   useEffect(() => {
-    const lenis = new Lenis({
-      smooth,
-      infinite,
+    const scrollGalleryRef = scrollGallery.current;
+    const scrollGalleryRefWidth = scrollGalleryRef.offsetWidth;
+    const scrollGalleryRefHeight = scrollGalleryRef.offsetHeight;
+    const scrollGalleryRefChildren = scrollGalleryRef.children;
+    const scrollGalleryRefChildrenLength = scrollGalleryRefChildren.length;
+
+    // on ajoute les images à la fin de la galerie
+    for (let i = 0; i < scrollGalleryRefChildrenLength; i++) {
+      scrollGalleryRef.appendChild(scrollGalleryRefChildren[i].cloneNode(true));
+    }
+
+    // on ajoute les images au debut de la galerie
+    for (let i = 0; i < scrollGalleryRefChildrenLength; i++) {
+      scrollGalleryRef.insertBefore(
+        scrollGalleryRefChildren[i].cloneNode(true),
+        scrollGalleryRef.firstChild
+      );
+    }
+
+    // on positionne la galerie au milieu
+    scrollGalleryRef.style.transform = `translate3d(-${scrollGalleryRefWidth}px, -${scrollGalleryRefHeight}px, 0)`;
+
+    // on ajoute un event listener sur la galerie
+    scrollGalleryRef.addEventListener("scroll", () => {
+      // on recupere la position de la galerie
+      const scrollGalleryRefScrollLeft = scrollGalleryRef.scrollLeft;
+      const scrollGalleryRefScrollTop = scrollGalleryRef.scrollTop;
+
+      // on verifie si la galerie est au debut ou a la fin
+      if (
+        scrollGalleryRefScrollLeft <= 0 ||
+        scrollGalleryRefScrollLeft >=
+          scrollGalleryRef.scrollWidth - scrollGalleryRefWidth
+      ) {
+        scrollGalleryRef.scrollLeft = scrollGalleryRefWidth;
+      }
+
+      if (
+        scrollGalleryRefScrollTop <= 0 ||
+        scrollGalleryRefScrollTop >=
+          scrollGalleryRef.scrollHeight - scrollGalleryRefHeight
+      ) {
+        scrollGalleryRef.scrollTop = scrollGalleryRefHeight;
+      }
     });
-
-    rafRef.current = (time) => {
-      lenis.raf(time);
-      requestAnimationFrame(rafRef.current);
-    };
-
-    requestAnimationFrame(rafRef.current);
-
-    return () => {
-      cancelAnimationFrame(rafRef.current);
-    };
-  }, [smooth, infinite]);
+  }, []);
 
   return (
-    <div
-      className="scroll-gallery lenis"
-      ref={rafRef}
-      data-lenis-scroll
-      data-lenis-prevent
-    >
-      {children}
+    <div className="scroll-gallery" ref={scrollGallery}>
+      {images_gallery.map((img) => (
+        <img src={img.src} key={shortid.generate()} />
+      ))}
     </div>
   );
 };
 
-const RepeatItems = () => {
-  const parentElRef = useRef(null);
-
-  useEffect(() => {
-    const items = [...parentElRef.current.children];
-    for (let i = 0; i <= 6 - 1; ++i) {
-      const cln = items[i].cloneNode(true);
-      parentElRef.current.appendChild(cln);
-    }
-  }, []);
-
-  return (
-    <ScrollGalleryImage smooth={true} infinite={true}>
-      <div
-        className="scroll-gallery lenis"
-        ref={parentElRef}
-        data-lenis-scroll
-        data-lenis-prevent
-      >
-        {images_gallery.map((img) => (
-          <img src={img.src} key={shortid.generate()} />
-        ))}
-      </div>
-    </ScrollGalleryImage>
-  );
-};
-
-export default RepeatItems;
+export default ScrollGalleryImage;
