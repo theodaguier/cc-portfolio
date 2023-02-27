d33881a21d7d45d580e2366267e00e101edeee8f
theodaguier <92515854+theodaguier@users.noreply.github.com>
2023-02-27 19:42:27 +0100

    Version 1 gallery

--- src/components/ScrollGalleryImage.jsx
+++ src/components/ScrollGalleryImage.jsx
@@ -1,67 +1,48 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import shortid from "shortid";
 import images_gallery from "../data/images_gallery.js";
 
 const ScrollGalleryImage = () => {
-  // cette galerie d'image importés depuis un fichier json
-  // cette galerie doit un etre un scroll vertical
-  // la galerie doit repeter à l'infini les images
-  // avec un effet de smooth scroll
-
+  const [images, setImages] = useState(images_gallery);
   const scrollGallery = useRef(null);
+  const imageHeight = 300; // ajustez la hauteur des images selon vos besoins
+  const [scrollOffset, setScrollOffset] = useState(0);
 
   useEffect(() => {
     const scrollGalleryRef = scrollGallery.current;
-    const scrollGalleryRefWidth = scrollGalleryRef.offsetWidth;
-    const scrollGalleryRefHeight = scrollGalleryRef.offsetHeight;
-    const scrollGalleryRefChildren = scrollGalleryRef.children;
-    const scrollGalleryRefChildrenLength = scrollGalleryRefChildren.length;
 
-    // on ajoute les images à la fin de la galerie
-    for (let i = 0; i < scrollGalleryRefChildrenLength; i++) {
-      scrollGalleryRef.appendChild(scrollGalleryRefChildren[i].cloneNode(true));
-    }
-
-    // on ajoute les images au debut de la galerie
-    for (let i = 0; i < scrollGalleryRefChildrenLength; i++) {
-      scrollGalleryRef.insertBefore(
-        scrollGalleryRefChildren[i].cloneNode(true),
-        scrollGalleryRef.firstChild
-      );
-    }
-
-    // on positionne la galerie au milieu
-    scrollGalleryRef.style.transform = `translate3d(-${scrollGalleryRefWidth}px, -${scrollGalleryRefHeight}px, 0)`;
-
-    // on ajoute un event listener sur la galerie
-    scrollGalleryRef.addEventListener("scroll", () => {
-      // on recupere la position de la galerie
-      const scrollGalleryRefScrollLeft = scrollGalleryRef.scrollLeft;
-      const scrollGalleryRefScrollTop = scrollGalleryRef.scrollTop;
-
-      // on verifie si la galerie est au debut ou a la fin
-      if (
-        scrollGalleryRefScrollLeft <= 0 ||
-        scrollGalleryRefScrollLeft >=
-          scrollGalleryRef.scrollWidth - scrollGalleryRefWidth
-      ) {
-        scrollGalleryRef.scrollLeft = scrollGalleryRefWidth;
-      }
+    const handleScroll = () => {
+      const { scrollTop, clientHeight, scrollHeight } = scrollGalleryRef;
 
       if (
-        scrollGalleryRefScrollTop <= 0 ||
-        scrollGalleryRefScrollTop >=
-          scrollGalleryRef.scrollHeight - scrollGalleryRefHeight
+        scrollTop + clientHeight >= scrollHeight &&
+        images.length < images_gallery.length * 2
       ) {
-        scrollGalleryRef.scrollTop = scrollGalleryRefHeight;
+        setImages((prevImages) => [...prevImages, ...images_gallery]);
       }
-    });
+    };
+
+    scrollGalleryRef.addEventListener("scroll", handleScroll);
+
+    return () => {
+      scrollGalleryRef.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
-    <div className="scroll-gallery" ref={scrollGallery}>
-      {images_gallery.map((img) => (
-        <img src={img.src} key={shortid.generate()} />
+    <div
+      className="scroll-gallery"
+      ref={scrollGallery}
+      style={{
+        transform: `translateY(-${scrollOffset}px)`,
+      }}
+    >
+      {images.map((img) => (
+        <img
+          src={img.src}
+          key={shortid.generate()}
+          style={{ height: `${imageHeight}px` }}
+        />
       ))}
     </div>
   );
